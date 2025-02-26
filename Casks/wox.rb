bbfe75d20ae5171322cacdcc3ea3ccdb4fd23830
GitHub Action <action@github.com>
2025-02-26 01:20:21 +0000

    Update Wox cask to v2.0.0-nightly

--- Casks/wox.rb
+++ Casks/wox.rb
@@ -4,12 +4,12 @@ cask "wox" do
     version "v2.0.0-nightly"
 
     on_arm do
-      sha256 "fadf8eeb0ce2623c9bc0235809cb6dec095d8eb1fce7c7a78b52e7b404b10f5f"
-      url "https://github.com/Wox-launcher/Wox/releases/download/v2.0.0-nightly/wox-mac-arm64-20250224-5e2c10.dmg"
+      sha256 "4b7879ff723c6c75e9cd08cc50a81ce40458b9cc70ba2f86653ca29a15c1cf09"
+      url "https://github.com/Wox-launcher/Wox/releases/download/v2.0.0-nightly/wox-mac-arm64-20250225-70527b.dmg"
     end
     on_intel do
-      sha256 "1d81b903c15b76f771815d1ea0fec558b48e8076e0ba1646d5ac27495f792d37"
-      url "https://github.com/Wox-launcher/Wox/releases/download/v2.0.0-nightly/wox-mac-amd64-20250224-5e2c10.dmg"
+      sha256 "cd072bb580c05f9fe942ab4772d1af038a9c1483582a7af75053f4610be8afe9"
+      url "https://github.com/Wox-launcher/Wox/releases/download/v2.0.0-nightly/wox-mac-amd64-20250225-70527b.dmg"
     end
   
     name "Wox"
