4d361554973b0b2dd476910ca115c7f54bf4a7f3
Grigory Petrov <grigoryvp@gmail.com>
2025-10-19 12:35:22 +0200

    Update Wox cask to v2.0.0-beta.5

--- Casks/wox.rb
+++ Casks/wox.rb
@@ -1,16 +1,16 @@
 cask "wox" do
     arch arm: "arm64", intel: "x86_64"
     
-    version "v2.0.0-beta.2"
+    version "v2.0.0-beta.5"
 
     on_arm do
-      sha256 "7e18d1e49aa8de280b84cd0060686abcaa2790bc2fe8b408058229c4edaf37d2"
-      url "https://github.com/Wox-launcher/Wox/releases/download/v2.0.0-beta.2/wox-mac-arm64.dmg",
+      sha256 "90707e62d3f3590b77ea85283cd9d93b297ed223f10bd813dadbbfdef431f8a9"
+      url "https://github.com/Wox-launcher/Wox/releases/download/v2.0.0-beta.5/wox-mac-arm64.dmg",
           verified: "github.com/Wox-launcher/Wox/"
     end
     on_intel do
-      sha256 "3c7d87d6e2f652805b94f4655b1d22613c20c01d765c41a02f139e3dafa449a6"
-      url "https://github.com/Wox-launcher/Wox/releases/download/v2.0.0-beta.2/wox-mac-amd64.dmg",
+      sha256 "8f1bbeb1d69a8e5d9e690835abcb5f61bf8419f710df26f0e1a75bfd9e0b095d"
+      url "https://github.com/Wox-launcher/Wox/releases/download/v2.0.0-beta.5/wox-mac-amd64.dmg",
           verified: "github.com/Wox-launcher/Wox/"
     end
   
